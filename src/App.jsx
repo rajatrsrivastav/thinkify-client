08b4a5c8815aa541597d3c570a7c7768594b1885
NSTKrishna <krishnagehlot936@gmail.com>
2025-07-27 15:48:03 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,55 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/homePage";
-import SubjectPage from "./pages/subjectPage"
+import SubjectPage from "./pages/subjectPage";
 import WorkSpace from "./pages/workspacePage";
 import SignUp from "./pages/signUpPage";
 import Login from "./pages/loginPage";
-import MainLayout from "./components/layout/mainLayout"
+import MainLayout from "./components/layout/mainLayout";
 
 // const router = createBrowserRouter(
 //   createRoutesFromElements( //v6
 //      <Router>
-    //   <Routes>
-    //     <Route element={<MainLayout />}>
-    //       <Route path="/" element={<HomePage />} />
-    //       <Route path="/auth/login" element={<LoginPage />} />
-    //       <Route path="/auth/signup" element={<SignUpPage />} />
-    //       <Route path="/:subject" element={<SubjectPage />} />
-    //     </Route>
-    //     <Route path="/:subject/:problemId" element={<WorkspacePage />} />
-    //   </Routes>
-    // </Router>
+//   <Routes>
+//     <Route element={<MainLayout />}>
+//       <Route path="/" element={<HomePage />} />
+//       <Route path="/auth/login" element={<LoginPage />} />
+//       <Route path="/auth/signup" element={<SignUpPage />} />
+//       <Route path="/:subject" element={<SubjectPage />} />
+//     </Route>
+//     <Route path="/:subject/:problemId" element={<WorkspacePage />} />
+//   </Routes>
+// </Router>
 //   )
 // );
 //v6.4
-const  router=createBrowserRouter([
+const router = createBrowserRouter([
   {
-    path:"/",
-    element:<MainLayout/>,
-    children:[
+    path: "/",
+    element: <MainLayout />,
+    children: [
       {
-        path:'/',
-        element:<HomePage/>
+        path: "/",
+        element: <HomePage />,
       },
       {
-        path:'/:subject',
-        element:<SubjectPage/>
+        path: "/:subject",
+        element: <SubjectPage />,
       },
       {
-        path:'/auth/login',
-        element:<Login/>
+        path: "/auth/login",
+        element: <Login />,
       },
       {
-        path:'/auth/SignUp',
-        element:<SignUp/>
-      }
-    ]
+        path: "/auth/SignUp",
+        element: <SignUp />,
+      },
+    ],
   },
   {
-    path:"/:subject/:problemId",
-    element:<WorkSpace />
-  }
-]
-)
+    path: "/:subject/:problemId",
+    element: <WorkSpace />,
+  },
+]);
 function App() {
   return <RouterProvider router={router} />;
 }
